3cceab919eb66064e0160154dc8e3443c4ea2078
carlos perez <carlosperez505@gmail.com>
2022-06-17 17:17:19 -0600

    add app.js file

--- cp3-develops/src/App.js
+++ cp3-develops/src/App.js
@@ -1,1 +1,36 @@
+import './App.css';
+import Cbr from './cbr.jpeg';
+import Github from './github.png';
+import Linkdin from './linkdin-icon-logo.png';
 
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={Linkdin} className="App-link"/>
+                <img src={Cbr} className="App-cbr"/>
+                <div>
+                    <p>
+                        hey hey
+                    </p>
+                </div>
+                <p>
+                    Happy Birthday
+                </p>
+                <p>
+                    Good morning React
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
