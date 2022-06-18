d1a8c0d700bba98c20e348c50e47b9117b99eb98
carlos perez <carlosperez505@gmail.com>
2022-06-18 11:50:31 -0600

    practicing my skills

--- cp3-develops/src/App.js
+++ cp3-develops/src/App.js
@@ -1,42 +1,16 @@
 
 import './App.css';
-import Sunset from './sunset.jpg'
-import Github from './github.png';
-import Linkdin from './linkdin-icon-logo.png';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={Sunset} className="App-link" alt=""/>
-                <img src={Linkdin} className="App-link" alt=""/>
+                <p>
+                    cp3Develops
+                </p>
 
-                <div>
-                    <p>
-                        hey hey
-                    </p>
-                </div>
-                <p>
-                    Happy Birthday
-                </p>
-                <p>
-                    Good morning React
-                </p>
-                <img src={Github} className="App-link" alt=""/>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+
             </header>
-            <body>
-            <p>
-                hello
-            </p>
-            </body>
         </div>
     );
 }
