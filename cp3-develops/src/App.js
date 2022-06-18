b51f1d10620992c1caf9c4f7ee728b894b9203ae
carlos perez <carlosperez505@gmail.com>
2022-06-18 07:02:28 -0600

    adding to file

--- cp3-develops/src/App.js
+++ cp3-develops/src/App.js
@@ -1,33 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Cbr from './cbr.jpeg';
 
-
-
-
-
-
+import Github from './github.png';
+import Linkdin from './linkdin-icon-logo.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <img src={Cbr} className="App-cbr" alt="App-cbr"/>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={Linkdin} className="App-link" alt=""/>
+
+                <div>
+                    <p>
+                        hey hey
+                    </p>
+                </div>
+                <p>
+                    Happy Birthday
+                </p>
+                <p>
+                    Good morning React
+                </p>
+                <img src={Github} className="App-link" alt=""/>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
